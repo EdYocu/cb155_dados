6fbc0b4c11b8adebaad22267e78277ec1eb25c80
Edgardo Yocupicio Ruiz <edgardoyocupicio@gmail.com>
2022-05-16 12:13:12 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,64 @@
+let número = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    número = randint(1, 6)
+    if (número == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (número == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (número == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (número == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (número == 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (número == 6) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
